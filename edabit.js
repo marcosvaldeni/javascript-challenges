da25a2abaa90a63b83f58781ba55eb8ac5222cd9
Marcos Valdeni Lucas <marcosvaldeni@gmail.com>
2026-06-06 15:22:40 +0100

    Refactor done array in edabit.js
    Removed irrelevant entries from the done array and added new entries.

--- edabit.js
+++ edabit.js
@@ -1,193 +1,21 @@
 const done = [
-  '32rk4qSmtrB6oJGyn', // irrelevant
-  'DCmM4Eo6GQfrJoKXc', // irrelevant
-  'DcmB9Ycm58FdkPe7k', // irrelevant
-  'PwpJNJiysvq3AuYJ8', // irrelevant
-  '8rhnqxJFiJm5tS4G7', // irrelevant
-  'Q3n42rEWanZSTmsJm', // irrelevant
-  'm6GYKsereS6JdARGW', // irrelevant
-  'r9uigYABnbPLY7zsz', // irrelevant
-  'S6zpG8RwDrxaFEQLE', // irrelevant
-  'ZqxDxzZK6Fm9uW6MF', // irrelevant
-  'fP7gFvDaBymoZrXFx', // irrelevant
-  'iDxwkarcJcmkDA8tq', // irrelevant
-  'X6PDfNfJwcB4TkQuQ', // irrelevant
-  'n2bFd2enCnHJkTwsK', // irrelevant
-  'r2MbSxquAGPnDkjQx', // irrelevant
-  'RQwdZmtrW8mCnuCMN', // irrelevant
-  'nEdLGbAZQ5LaiumP6', // irrelevant
-  'g6b9HqkXqWu6GpfTo', // irrelevant
-  'rGsgEswWuW339yNxY', // irrelevant
-  'JfB9mWmbwYHbupxCB', // irrelevant
-  'qMr6wYGr6NaXAPQGF', // irrelevant
-  'iBQYbSHZGhpktLRdn', // irrelevant
-  'EzbfiquDoAc2Zc9FL', // irrelevant
-  'exMRkWdmmc6hccYiS', // irrelevant
-  '4LnwAC3kgTctc7r3k', // irrelevant
-  'WacpbmmhwWJ2Wgmy9', // irrelevant
-  '8A9E8LdE6zXcmPX3W', // irrelevant
-  'k5k3aXWp7aibft74n', // irrelevant
-  'j3EoCMaD3LrEYtx78', // irrelevant
-  'RXNybvGHLuvFiWKvq', // irrelevant
-  'bADsAhtb5g6pu5WYM', // irrelevant
-  '77zq4RhMgWup22H6d', // irrelevant
-  'yHGowWucg3k2kJdZ4', // irrelevant
-  'uE9AJ4sSrrpSASMpu', // irrelevant
-  'YMWDcSuYwYvve3HZj', // irrelevant
-  '5erCDJ8eJDrXkmwTK', // irrelevant
-  '4SmqXvQtL6qRgzfha', // irrelevant
-  'd9suvbchE2bnHNQuK', // irrelevant
-  'QkvDge63crdGnMfvM', // irrelevant
-  'KSTkFSnaYBJdo6PHx', // irrelevant
-  'ghbHrRnRiDz9fvQNF', // irrelevant
-  'BGvTMfwxYDRbtaTJ3', // irrelevant
-  'FipbQSYquQLPZ8QXG', // irrelevant
-  'JesaFi5ntBEbGT8bu', // irrelevant
-  'QSnaSH5S3oxZkwcNc', // irrelevant
-  'vJCZmgvvDjehyDcDK', // irrelevant
-  '8Qg78sf5SNDEANKti', // irrelevant
-  'CCGBig9eRPFzAHv46', // irrelevant
-  '4iCsexZgmDEMMxj46', // irrelevant
-  '9MjEpkL7yAjAqiH58', // irrelevant
-  'ZNwHGgHvsdnYwJ5WK', // irrelevant
-  'MwLp6s75F6S5wRrEJ', // irrelevant
-  '3xqP79TrHiwnwGizj', // irrelevant
-  'BEHiM4RTmhcLX7Cq6', // irrelevant
-  'NEPFa3NZSd8iiF3ps', // irrelevant
-  '8fnEAEj5vKNqAXNYA', // irrelevant
-  'dWNPtNeQgyn6mS8FA', // irrelevant
-  'T9nCQgLQJ9Pi9a9kH', // irrelevant
-  'Qbi3GuXjwW5fsoKRn', // irrelevant
-  'qYAhaD8fTbHWNe9oT', // irrelevant
-  'bcM99TYJmRv5JnZaA', // irrelevant
-  'v9DwaeR6NQoapEvHh', // irrelevant
-  'W8z2EghwmjQYhwM2D', // irrelevant
-  'R3AYf3eTdATXTQTdZ', // irrelevant
-  'g66EJsbdnSDayyEcK', // irrelevant
-  'Hp9FitGXpKqdCW6Fd', // irrelevant
-  '2SJX9ha6W2bYkzSNX', // irrelevant
-  'LMhRTq3hccz2D9Lax', // irrelevant
-  'uJ9K3HtbjHoDGXKhg', // irrelevant
-  '4vvFr9ZTK2AdyswXH', // irrelevant
-  'havnWL4BimvoaXNMn', // irrelevant
-  'BA8Pom669icAdkdgj', // irrelevant
-  'v5TorwH9tiNtddSY3', // irrelevant
-  'hwWLHcDoPZmXcGHyd', // irrelevant
-  'MWRiXba7bvi792SmQ', // irrelevant
-  'H7cs3TS87sFX72mjF', // irrelevant
-  'KESyfS3A5vB2jkxFR', // irrelevant
-  'HTbRYCmPwfZGnBcxW', // irrelevant
-  'L76DRaqDXkBJJ7tBr', // irrelevant
-  '5232asan2yFuMGnHq', // irrelevant
-  'sq7ZNMhAZ5ao8BMum', // irrelevant
-  'Y46Xp2pcvTB77bmdD', // irrelevant
-  'fBJyQSe5Jmbm9hPAG', // irrelevant
-  'PTiLYyb4A69KZtBCg', // irrelevant
-  'MvZK536X7fyrWH8Qc', // irrelevant
-  'XnJ24rWW7iJkNrtsh', // irrelevant
-  'Q2j5FTFtsk7PdzrQk', // irrelevant
-  'nhXofMMyrowMyr9Nv', // irrelevant
-  '6AnQqiEjkJdZrWhPS', // irrelevant
-  'wAdE9te55cowBLcPs', // irrelevant
-  'j7yQbF3J3rToHsDBP', // irrelevant
-  '3CaszbdZYGN4otQD8', // irrelevant
-  'qMr6wYGr6NaXAPQGF', // irrelevant
-  'NKknKNfeaJxLDfJuZ', // irrelevant
-  '9TWhGHCeytQEBCbc9', // irrelevant
-  'pKSS2rNDiXh4MoNpe', // irrelevant
-  'fh9i7k936rvfjnCYR', // irrelevant
-  'GwvwXHWCThHZrR7xu', // irrelevant
-  'bL7hSc6Zh4zZJzGmw', // irrelevant
-  'ks3vMrqdnW3CQ3F4i', // irrelevant
-  'nkp5xkZwbBH5Ape8b', // irrelevant
-  '8q54MKnRrm89pSLmW', // irrelevant
-  'NAQhEoxbofPidLxm9', // irrelevant
-  'QaApgtePE6QrCZ64o', // irrelevant
   'ARr5tA458o2tC9FTN', // irrelevant
   '3LpBLgNRyaHMvNb4j', // irrelevant
-  'BFnsRqe8PFvEwcRNt', // irrelevant
-  '654ABGmNS5GqscE8C', // irrelevant
-  'jipHTDkabftop5irE', // irrelevant
-  'sWAEoTbXA4bexBPb6', // irrelevant
-  'BLJ5SyhMoZD892G7w', // irrelevant
-  'CzPEsTuXfE8J8vknX', // irrelevant
-  'ZZ82YaHmPZRewQNYH', // irrelevant
-  'pEzxi4MqHGrAi7ZdA', // irrelevant
-  'z6Pxiw289JtaE2ndL', // irrelevant
-  'NMdKxEradTmpNnomZ', // irrelevant
-  'MQL7KSftPQzrxdJw6', // irrelevant
-  'w7BYrKiiszu5eJkuc', // irrelevant
-  'TihiK4mBiYsdGuR5H', // irrelevant
-  'QWmvQsrSuQRmEN8ne', // irrelevant
-  'wJuXYhgetMtD4Gezj', // irrelevant
-  'rTDLyF5itdoubs9ka', // irrelevant
-  'STfAEAE5fjNmFCsvf', // irrelevant
-  'M9EfvGwTqvSZ5XbRM', // irrelevant
-  'wpHyrxbSaYxLeXT6L', // irrelevant
-  'pzmTaGsP6FWRNBF9y', // irrelevant
-  'B9yPEStmax7Jxb9Dj', // irrelevant
-  'bvPBzNq9FutDb2X6J', // irrelevant
-  '9cB7BgGkeDcKwwhxX', // irrelevant
-  'HBNwoHQJn5uTWJCgM', // irrelevant
-  'ciuEF8N3NNegxbasd', // irrelevant
-  'mN98zKeSSLnXwKz8C', // irrelevant
-  '7hrjegxToTSF4zAoM', // irrelevant
-  '8xAvdyphm83tzKCKt', // irrelevant
-  'yks7uNNSRJFWDuj3m', // irrelevant
-  'MT5SbF9Xg5Dku6bqL', // irrelevant
-  'pDc7MLcGYzhCW6wRf', // irrelevant
-  '5cGbZP8N9mQXzDwML', // irrelevant
-  'xwNSxpGzwRh3brHQf', // irrelevant
-  'BsCHQ6fyzmrTYLNsR', // very easy
-  'FvXHdrhjLaof47Qt3', // very easy
-  'kJQYTCCWSnzhXG9dn', // very easy
-  '7JBTN4TbaxJQMdX9W', // very easy
-  '9ApjPggCLGiPt573m', // very easy
-  'SwyjHvkqwwQ2iJsoS', // very easy
-  '9YPqr4rNaYy8Ynrry', // very easy
-  'NkMTBCxMkt4meHpa4', // very easy
-  'RxsaajpPai3CB9G9e', // very easy
-  '8zbTPvexR9Aei3Csp', // very easy
-  '2v2FPyt9bhkLm4st6', // very easy
-  'y6dMjvNaPXXNKeBRQ', // very easy
-  '9mKB2XJJ9gYgjms4Z', // very easy
-  'dgf76hmQ5Yw4FATFi', // very easy
-  'izss6QT59oH72uky3', // very easy
-  'XKX5NHT92okxXCKW7', // very easy
-  '3BqLw3Rp85Y94nAho', // very easy
-  'HFBYPP8gSRn2n9qrQ', // very easy
-  'rThajiu3LF7qiKpv9', // very easy
-  'L395zsraDGkjQCSpC', // very easy
-  'mptt94wPwNxqJJzgr', // very easy
-  'qcw8QBqa4amN2x4q4', // easy
-  'qgS7XZjvL8wEChNvq', // easy
-  '5L368ypvh5ZZ2HJSL', // easy
-  '9wL6dXj2tKNCe5pFz', // easy
-  '3pJCxeSiKFC9gKtYt', // easy
-  'zHutBxQaWvzhzGgxL', // easy
-  'webH9q8JMHc6SmTpY', // easy
-  'iuLDXd4mTgcCeTjhM', // easy
-  'ado29iTz4EGfzY2EP', // easy
-  'AhFCvDdQeA7TnKSqe', // easy
-  'qWhoswdp35f8xzqGw', // easy
-  'WR3Q72Jbv7Kyzu6AT', // easy
-  'xn3DTHgvGspvQxmRQ', // easy
-  '7fLxcoScQJBhMFvKK', // easy
-  'xr4Xf28xeSrc7XmCF', // easy
-  '4MSbtYFBiRtxHEkY8', // easy
-  'dNZmjoi3Lip2j4emx', // easy
-  'R6DX8wKMufT5hLRuK', // easy
-  'reR3jdfJnREauJj8d', // easy
-  'Gpy2qSFnfhGJnWMMj', // easy
-  'udRB96tKMz43nhGkS', // 👍 easy
-  '4weFPnpgS2S5aw4aN', // 👍 easy
-  'rqu2efZF7N2MEYDWk', // 👍 easy
-  'EEWcSX7Rz5cWzes5d', // 👍 easy
-  'fzF6JqF7JjtJok9jY', // 👍 easy
-  '5RiZh6yJKeGEo4P3z', // 👍👍 easy
-  'tSBCQQ8w9dAAZuKio', // 👍👍 easy
-  'aJzvBZgp8nzwSk94A', // 👍👍 easy
-  'dzdoYrTL5vws4dFud', // 👍👍 easy
+  '8q54MKnRrm89pSLmW', // irrelevant
+  'NAQhEoxbofPidLxm9', // irrelevant
+  '3CaszbdZYGN4otQD8', // irrelevant
+  'bL7hSc6Zh4zZJzGmw', // irrelevant
+  'j7yQbF3J3rToHsDBP', // irrelevant
+  'QaApgtePE6QrCZ64o', // irrelevant
+  'wAdE9te55cowBLcPs', // irrelevant
+  '6AnQqiEjkJdZrWhPS', // irrelevant
+  'nhXofMMyrowMyr9Nv', // irrelevant
+  'Q2j5FTFtsk7PdzrQk', // irrelevant
+  'XnJ24rWW7iJkNrtsh', // irrelevant
+  'MvZK536X7fyrWH8Qc', // irrelevant
+  'ks3vMrqdnW3CQ3F4i', // irrelevant
+  'PTiLYyb4A69KZtBCg', // irrelevant
+  'fBJyQSe5Jmbm9hPAG', // irrelevant
 ];
 
 const listExercises = document.querySelectorAll('[role="listitem"]');
